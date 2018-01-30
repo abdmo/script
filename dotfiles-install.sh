d706191063e07210411bc670ecfb84be07d21c76
Fattah Zaki <fattahzaki@gmail.com>
2018-01-30 11:59:13 +0800

    dotfiles-install.sh: corrected wrong repository name

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -1,6 +1,6 @@
-git clone --bare https://github.com/abdmo/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/abdmo/dotfiles.git $HOME/.dotfiles
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 mkdir -p .dotfiles-backup
 dotfiles checkout
